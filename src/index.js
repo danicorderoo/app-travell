f980039f250783d19c5feb04b7c0dd27c6cbd3c6
danicorderoo <soydcordero@gmail.com>
2023-01-14 10:41:08 -0400

    deploy url

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api-travell.onrender.com";
+axios.defaults.baseURL = "https://api-travell.onrender.com/";
 
 ReactDOM.render(
   <Provider store={store}>
